4efce536f1d8c349c6e5199a2bd28921db1aea1f
ljreader <l.j.reader@hotmail.com>
2018-04-10 11:34:56 -0600

    4 pane initial

---
+++ arivo/assets/javascript/analytics.js
@@ -0,0 +1,3 @@
+https://www.chase.com/online/Home/images/wa01.gif?log=1&wa_cb=1523322372222.637750&wa_uri=https%3A%2F%2Fautofinance.chase.com%2Fauto-finance%2Fhome&wa_pt=Chase%20Auto%20Finance%20%7C%20Auto%20Loan%20Calculator%20%7C%20Chase.com&wa_sr=1536x864&wa_br=4608x2203&wa_fv=Not%20enabled
+
+// https://www.google-analytics.com/analytics.js
---
+++ arivo/assets/javascript/findCar.js
@@ -0,0 +1,154 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chase Auto Finance - Page Not Found</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet" />
+    <style>
+        body { font-family: Arial; min-width: 320px; }
+        .img-responsive { margin: 0 auto; }
+        .header img { margin-top: 20px; }
+        .header { height: 82px; text-align: center; background-image: url('/img/header_gradient_desktop.png'); background-position-y: -15px; }
+        #maincontent { text-align: left; font-size: 24px; line-height: 48px; color: #717171; }
+        #maincontent { max-width: 1161px; margin: 0 auto; }
+        #maincontent .subheader { font-size: 32px; }
+        #maincontent p { margin-bottom: 15px; }
+        h1 { font-size: 48px; color: #717171; line-height: 1.42; margin: 50px 0 20px; }
+        .footer { margin-top: 30px; padding-top: 15px; }
+        .footer, .footer a { font-size: 11px; color: #717171; text-align: center; }
+
+        @media (max-width: 767px) {
+            h1 { font-size: 30px; margin-top: 30px; }
+            .header { height: 62px; }
+            .header img { width: 160px; }
+            #maincontent { font-size: 14px; line-height: 1.42; }
+            #maincontent .subheader { font-size: 20px; }
+            .footer { margin-top: 0; }
+            .footer, .footer a { font-size: 12px; color: #717171; line-height: 2; }
+            .footer ul { padding: 0 15px; }
+        }
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row header">
+            <img src="/img/chase_logo.png" alt="chase logo" />
+        </div>
+        <div id="maincontent">
+            <div class="description">
+                <h1>Oops, you found it!</h1>
+                <p class="subheader">The one page we never want you to see.</p>
+                <p>Now, let's help you find what you're looking for:</p>
+                <ul>
+                    <li><a href="/auto-finance/car-buying-service">Find and price a car</a></li>
+                    <li><a href="/auto-finance/auto-loans">Finance a car</a></li>
+                    <li><a href="/auto-finance/auto-refinance">Refinance your car loan</a></li>
+                </ul>
+                <p>Still can't find what you're looking for? Try visiting our <a href="/auto-finance/sitemap">site map</a>.</p>
+            </div>
+            <hr />
+        </div>
+        <div class="row footer">
+            <div class="col-xs-12">
+                <ul class="list-inline">
+                    <li><a id="FooterNav_PrivacyNotice" href="https://www.chase.com/resources/privacy-security" target="_blank" class="chaseanalytics-track-link" data-pt-name="ft_privnotice">Privacy</a></li>
+                    <li><a id="FooterNav_PrivacyNotice" href="https://www.chase.com/resources/privacy-security" target="_blank" class="chaseanalytics-track-link" data-pt-name="ft_privnotice">Security</a></li>
+                    <li><a id="FooterNav_TermsofUse" href="https://www.chase.com/digital/resources/terms-of-use" target="_blank" class="chaseanalytics-track-link" data-pt-name="ft_termsofuse">Terms of Use</a></li>
+                    <li><a id="FooterNav_Accessibility" href="https://www.chase.com/digital/resources/accessibility " target="_blank" class="chaseanalytics-track-link" data-pt-name="ft_canada">Accessibility</a></li>
+                    <li>
+                        <a id="Legal_AdChoices" href="http://www.aboutads.info/choices/" data-pt-name="ft_adchoices" data-remote="false" data-target="#ThirdPartyPopUp" data-toggle="modal" class="speedbump weblinkingPopup">AdChoices<span class="sr-only"> Opens Overlay </span></a>
+                    </li>
+                </ul>
+                <!--http://www.aboutads.info/choices/-->
+            </div>
+            <div class="col-xs-12">
+                <div>
+                    &copy;&nbsp;<span id="date"></span> JPMorgan Chase Bank, N.A. Member FDIC
+                </div>
+            </div>
+        </div>
+    </div>
+    <link href="/css/thirdparty.css" rel="stylesheet" />
+
+    <div class="generic-modal third-party-modal modal fade" id="ThirdPartyPopUp" tabindex="-1" role="dialog" aria-labelledby="ThirdPartyPopup">
+        <span class="sr-only">Begin external link overlay.</span>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="thirdPartyTitle">
+                        You're now leaving Chase
+                    </div>
+                    <div class="thirdPartyCopy">Chase's website terms, privacy and security policies don't apply to the site you're about to visit. Please review its website terms, privacy and security policies to see how they apply to you.</div>
+                    <div class="thirdPartyButtons clearfix">
+                        <a href="javascript:void(0);" id="thirdPartyCancelLink" class="button cancel-btn" data-dismiss="modal">Cancel<span class="sr-only">Close Overlay </span></a>
+                        <a id="thirdPartyProceedLink" href="http://www.aboutads.info/choices/" class="button proceed-btn chaseanalytics-opt-exlnk" target="_blank">Proceed<span class="sr-only">Opens new window </span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <span class="sr-only">End external link overlay.</span>
+    </div>
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script>
+
+        /* 3rd party modal script BEGIN */
+        var initThirdPartyModal = function () {
+            $('.speedbump').each(function () {
+                $(this).append("<span class='sr-only'> Opens Overlay </span>");
+            });
+
+            var $modal3rdParty = $('#ThirdPartyPopUp');
+
+            $modal3rdParty.on('show.bs.modal',
+                function (e) {
+                    var $link = $(e.relatedTarget);
+                    var thisHref = $link.attr('href');
+                    var $proceedLink = $('#thirdPartyProceedLink');
+
+                    $proceedLink.on('click',
+                        function () {
+
+                            //$modal3rdParty.modal('hide');
+                            $('#thirdPartyCancelLink').click();
+                        }).attr('href', thisHref);
+
+                    $modal3rdParty.css('visibility', 'visible').fadeIn(function () {
+                        $proceedLink.focus();
+                    });
+
+                });
+
+            var $lastClicked;
+            $('a, button').click(function () {
+                $lastClicked = $(this);
+            });
+
+            $modal3rdParty.on('hidden.bs.modal',
+                function () {
+                    if ($lastClicked) {
+                        $($lastClicked).focus();
+                    }
+                });
+        }();
+        /* 3rd party modal END */
+    </script>
+
+    <script>
+        var dateEl = document.getElementById('date'), date = new Date().getFullYear();
+        dateEl.innerHTML = date;
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-91178629-3', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+
+</body>
+</html>
