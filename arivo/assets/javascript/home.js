cd9069c08f8f4507b01fe254eeb5b0746d438a85
ljreader <l.j.reader@hotmail.com>
2018-04-12 18:18:28 -0600

    all links working

--- arivo/assets/javascript/home.js
+++ arivo/assets/javascript/home.js
@@ -1,17 +1,17 @@
 // https://www.chase.com/apps/services/tags/https/autofinance.chase.com/auto-finance/home
 CHASE.TagManager.processTags (
-{
-"jsonScriptArray": [
-{
-"mappingID": "601761472",
-"urls": [
-"https://www.facebook.com/tr?id=1162808973818489&ev=Purchase&noscript=1",
-"https://analytics.twitter.com/i/adsct?txn_id=nymro&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0",
-"https://t.co/i/adsct?txn_id=nymro&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0",
-""
-],
-"fireOnce": false
-}
-]
-}
-)
+	{
+	"jsonScriptArray": [
+	{
+	"mappingID": "601761472",
+	"urls": [
+	"https://www.facebook.com/tr?id=1162808973818489&ev=Purchase&noscript=1",
+	"https://analytics.twitter.com/i/adsct?txn_id=nymro&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0",
+	"https://t.co/i/adsct?txn_id=nymro&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0",
+	""
+	],
+	"fireOnce": false
+	}
+	]
+	}
+	)
